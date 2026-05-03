93b151aa02af113845d169fc65c3cf4103cb60eb
Simon Steinschaden <simon.steinschaden@outlook.at>
2026-05-04 00:00:49 +0200

    feat: Add new DTOs, enums, models, mappers, and validators for import functionality

---
+++ src/Enset.Application/Imports/Enums/RawDataObjectType.cs
@@ -0,0 +1,7 @@
+public enum RawDataObjectType
+{
+    Excel,
+    Xml,
+    Csv,
+    Zoho
+}
---
+++ src/Enset.Application/Imports/Models/RawDataObject.cs
@@ -0,0 +1,6 @@
+public class RawDataObject
+{
+    public Guid Id { get; set; }
+    public RawDataObjectType Type { get; set; }
+    public string FilePath { get; set; }
+}

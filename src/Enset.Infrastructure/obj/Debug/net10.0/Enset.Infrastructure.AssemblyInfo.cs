656604c0d816a4460ecc18c76344717d5fe11090
Simon Steinschaden <simon.steinschaden@outlook.at>
2026-05-04 01:05:10 +0200

    Refactor clean architecture and import foundation

--- src/Enset.Infrastructure/obj/Debug/net10.0/Enset.Infrastructure.AssemblyInfo.cs
+++ src/Enset.Infrastructure/obj/Debug/net10.0/Enset.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Enset.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+755f08e8ddeceb09d42ff0d1e08ae22445242cb7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66a983f763cb16348932882c73a43885550431d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Enset.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Enset.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

66a983f763cb16348932882c73a43885550431d2
Simon Steinschaden <simon.steinschaden@outlook.at>
2026-05-04 01:04:25 +0200

    Update Enset.Application and Enset.Domain assemblies and related files
    - Updated AssemblyInformationalVersion in Enset.Application and Enset.Domain to "1.0.0+755f08e8ddeceb09d42ff0d1e08ae22445242cb7".
    - Modified sourcelink.json files for both projects to reflect the new commit hash.
    - Rebuilt Enset.Application and Enset.Domain, resulting in changes to DLL and PDB files.
    - Updated various cache files related to assembly references and compilation inputs.
    - Ensured all binary files in the Debug directory are up to date with the latest changes.

--- src/Enset.Domain/obj/Debug/net10.0/Enset.Domain.AssemblyInfo.cs
+++ src/Enset.Domain/obj/Debug/net10.0/Enset.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Enset.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58b859e13a94734f130d59e4ce9ff34758489004")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+755f08e8ddeceb09d42ff0d1e08ae22445242cb7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Enset.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Enset.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

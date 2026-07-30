a51ad279e552805bf4c90ad26c35d10808df344a
Simon Steinschaden <simon.steinschaden@outlook.at>
2026-07-30 17:41:40 +0200

    Architectur Review

--- src/Enset.Application/obj/Debug/net10.0/Enset.Application.AssemblyInfo.cs
+++ src/Enset.Application/obj/Debug/net10.0/Enset.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Enset.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c81f66b82508b5f5d460dab881dd70f756bb9cd1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cac8de59b58327608beae4ef4cf688ccd089f71c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Enset.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Enset.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
